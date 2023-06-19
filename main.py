cfec8f05a4bc4ee0e13d3af40c7178cd5d48561f
andrn92 <georgkartashov88@gmail.com>
2023-06-19 09:30:45 +0300

    first commit

--- main.py
+++ main.py
@@ -60,65 +60,68 @@ def create_tables(engine):
     Base.metadata.create_all(engine)
 
 
-DSN = 'postgresql://{0}:{1}@localhost:5432/{2}'.format(login, password, name_base)
-
-engine = sqlalchemy.create_engine(DSN)
-create_tables(engine)
-
-Session = sessionmaker(bind=engine)
-session = Session()
-
-author1 = Publisher(name="Dostoevsky")
-author2 = Publisher(name="Gogol")
-author3 = Publisher(name="Pushkin")
-
-book1 = Book(title="Captain's daughter", publisher=author3)
-book2 = Book(title="Eugene Onegin", publisher=author3)
-book3 = Book(title="Crime and punishment", publisher=author1)
-book4 = Book(title="Idiot", publisher=author1)
-book5 = Book(title="Auditor", publisher=author2)
-book6 = Book(title="Dead souls", publisher=author2)
-
-shop1 = Shop(name='Labirint')
-shop2 = Shop(name='Ozon')
-shop3 = Shop(name='Wildberries')
-
-stock1 = Stock(books=book5, shops=shop1, count=2)
-stock2 = Stock(books=book2, shops=shop1, count=1)
-stock3 = Stock(books=book1, shops=shop3, count=3)
-stock4 = Stock(books=book6, shops=shop2, count=2)
-stock5 = Stock(books=book4, shops=shop1, count=1)
-stock6 = Stock(books=book3, shops=shop3, count=3)
-
-sale1 = Sale(price=500, date_sale='2020-05-19', stocks=stock1, count=1)
-sale2 = Sale(price=700, date_sale='2020-08-09', stocks=stock1, count=2)
-sale3 = Sale(price=800, date_sale='2020-10-15', stocks=stock2, count=1)
-sale4 = Sale(price=600, date_sale='2020-11-09', stocks=stock3, count=2)
-sale5 = Sale(price=600, date_sale='2020-12-04', stocks=stock6, count=3)
-sale6 = Sale(price=900, date_sale='2021-01-14', stocks=stock4, count=2)
-sale7 = Sale(price=600, date_sale='2021-06-24', stocks=stock5, count=3)
-
-session.add_all([author1, author2, author3])
-session.add_all([book1, book2, book3, book4, book5, book6])
-session.add_all([shop1, shop2, shop3])
-session.add_all([stock1, stock2, stock3, stock4, stock5, stock6])
-session.add_all([sale1, sale2, sale3, sale4, sale5, sale6, sale7])
-session.commit()  
-
-
 if __name__ == '__main__':
+    DSN = 'postgresql://{0}:{1}@localhost:5432/{2}'.format(login, password, name_base)
+
+    engine = sqlalchemy.create_engine(DSN)
+    create_tables(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    author1 = Publisher(name="Dostoevsky")
+    author2 = Publisher(name="Gogol")
+    author3 = Publisher(name="Pushkin")
+
+    book1 = Book(title="Captain's daughter", publisher=author3)
+    book2 = Book(title="Eugene Onegin", publisher=author3)
+    book3 = Book(title="Crime and punishment", publisher=author1)
+    book4 = Book(title="Idiot", publisher=author1)
+    book5 = Book(title="Auditor", publisher=author2)
+    book6 = Book(title="Dead souls", publisher=author2)
+
+    shop1 = Shop(name='Labirint')
+    shop2 = Shop(name='Ozon')
+    shop3 = Shop(name='Wildberries')
+
+    stock1 = Stock(books=book5, shops=shop1, count=2)
+    stock2 = Stock(books=book2, shops=shop1, count=1)
+    stock3 = Stock(books=book1, shops=shop3, count=3)
+    stock4 = Stock(books=book6, shops=shop2, count=2)
+    stock5 = Stock(books=book4, shops=shop1, count=1)
+    stock6 = Stock(books=book3, shops=shop3, count=3)
+
+    sale1 = Sale(price=500, date_sale='2020-05-19', stocks=stock1, count=1)
+    sale2 = Sale(price=700, date_sale='2020-08-09', stocks=stock1, count=2)
+    sale3 = Sale(price=800, date_sale='2020-10-15', stocks=stock2, count=1)
+    sale4 = Sale(price=600, date_sale='2020-11-09', stocks=stock3, count=2)
+    sale5 = Sale(price=600, date_sale='2020-12-04', stocks=stock6, count=3)
+    sale6 = Sale(price=900, date_sale='2021-01-14', stocks=stock4, count=2)
+    sale7 = Sale(price=600, date_sale='2021-06-24', stocks=stock5, count=3)
+
+    session.add_all([author1, author2, author3])
+    session.add_all([book1, book2, book3, book4, book5, book6])
+    session.add_all([shop1, shop2, shop3])
+    session.add_all([stock1, stock2, stock3, stock4, stock5, stock6])
+    session.add_all([sale1, sale2, sale3, sale4, sale5, sale6, sale7])
+    session.commit() 
+
     while True:
         name = input('Enter name publisher: ')
         if name in ['Pushkin', 'Dostoevsky', 'Gogol']:
             break
         else:
             print("This publisher is not in our stores!")
+    print('-----------------------------------------------------')
     q = session.query(Publisher).filter(Publisher.name == name)
     for p in q.all():
         for b in p.books:
             for st in b.stock:
                 for sl in st.sale:
                     print(b.title, '|', st.shops.name, '|', sl.date_sale, '|', sl.price)
+    print('-----------------------------------------------------')
+
+    
 
 
     
